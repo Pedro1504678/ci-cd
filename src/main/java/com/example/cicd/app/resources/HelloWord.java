6e86950011ff00294c43cd21af7ef3236e62a38d
Pedro Alexandre <pedroalexandre.bastos@hotmail.com>
2026-01-18 16:33:05 -0300

    fix resource name

--- src/main/java/com/example/cicd/app/resources/HelloWord.java
+++ src/main/java/com/example/cicd/app/resources/HelloWord.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWord {
 
-    @GetMapping("/hello-word")
+    @GetMapping("/hello-world")
     public String getMessage() {
         return "Hello World";
     }
